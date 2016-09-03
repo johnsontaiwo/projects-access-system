e887e7451fac6a5d9211508e94d96816ab42c351
johnsontaiwo <josseytee@gmail.com>
2016-09-02 22:19:22 -0400

    routes to login page for the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
 
 
+get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      erb :'projects/homepage'
+    else
+      redirect to '/login'
+    end
+  end
+
 end
