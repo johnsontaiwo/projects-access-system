09118db9c779820a33432cfe69036e93e4aebae7
johnsontaiwo <josseytee@gmail.com>
2016-09-02 22:11:07 -0400

    route to show all contractors

--- app/controllers/contractors_controller.rb
+++ app/controllers/contractors_controller.rb
@@ -1,3 +1,16 @@
 class ContractorsController < ApplicationController
 
+get '/contractors' do
+  @contractors = Contractor.all
+  if !@contractors.empty?
+  erb :'/contractors/show_contractors'
+  else
+    redirect to "/contractors/new"
+  end
+end
+
+
+
+
+
 end
